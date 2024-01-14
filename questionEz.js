36166b351c50456286c368201d12bf81bb18487e
Sdanielgg <40220220@esmad.ipp.pt>
2024-01-14 20:33:25 +0000

    pequenas correções mas ainda existem erros

--- questionEz.js
+++ questionEz.js
@@ -3,17 +3,19 @@ var answer1 = document.getElementById("answer1");
 var answer2 = document.getElementById("answer2");
 var answer3 = document.getElementById("answer3");
 var answer4 = document.getElementById("answer4");
+var result = document.getElementById("Result");
+let wrong,correct
 let list=[0,1,2,3]
-let j =Math.floor(Math.random() * (4));
-
+let currentQuestion=0
 let questions = [
     ["Em que continente fica Guiné Equatorial?", "América", "Asia", "Oceania", "Africa", "Africa"],
-    ["text1", "question1", "question2", "question3", "question4", "correctQuestion"],
-    ["text2", "question1", "question2", "question3", "question4", "correctQuestion"],
-    ["text3", "question1", "question2", "question3", "question4", "correctQuestion"],
+    ["text1", "question1", "question2", "question3", "question4", "question1"],
+    ["text2", "question1", "question2", "question3", "question4", "question2"],
+    ["text3", "question1", "question2", "question3", "question4", "question3"],
 ];
 
-function showCurrentQuestion() {
+
+function showCurrentQuestion(j) {
     questionText.innerHTML=questions[j][0];
     answer1.innerHTML = questions[j][1];
     answer2.innerHTML = questions[j][2];
@@ -23,16 +25,37 @@ function showCurrentQuestion() {
 function checkAnswer(answer){
     if(answer.innerHTML==questions[j][5]){
         console.log("acertou!")
+        currentQuestion++
+        correct++
+        run()
+    }else{
+        console.log("erraste")
+        wrong++
     }
 }
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+    const h = Math.floor(Math.random() * (i + 1));
+    [array[i], array[h]] = [array[h], array[i]];
     }
 return array;
 }
-shuffleArray(list)
-console.log(list)    
-// Call the function to show the current question
-showCurrentQuestion();
+function run()
+{
+    if (currentQuestion<3){
+        list.pop()
+        j=list[list.length-1]
+        showCurrentQuestion(j)
+    }else{
+        console.log("hello")
+        result.style.display="block"
+    }
+}
+function start(){
+    shuffleArray(list)
+    j=list[list.length-1]
+    showCurrentQuestion(j)
+
+}
+start()
+console.log(list)
